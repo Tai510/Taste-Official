f1b7f99a98722cb9bf37923b5c0dececb004ea65
Tashi Woeser <tashiwoeser@Tashis-MacBook-Pro.local>
2020-11-26 16:18:37 -0800

    added logo and fixed navbar

--- src/App.js
+++ src/App.js
@@ -10,6 +10,30 @@ import Contact from './Components/Contact/Contact';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import styled from 'styled-components';
 import Footer from './Components/Footer/Footer';
+import Images from '../src/Images/taste.logo.jpg';
+
+
+const TasteLogo = styled.img`
+    height: 100px;
+    @media only screen 
+and (min-device-width : 320px) 
+and (max-device-width : 568px) {
+    height: 56px;
+    margin: auto;
+    margin-left: 10px;
+    margin-top: 10px;
+ }
+
+ @media only screen 
+    and (device-width : 375px) 
+    and (device-height : 812px) 
+    and (-webkit-device-pixel-ratio : 3) {
+    height: 70px;
+    margin: auto;
+    margin-left: 10px;
+    margin-top: 10px;
+     }
+`;
 
 
 
@@ -23,7 +47,10 @@ function App() {
   return (
     <Main className="App">
       <Router>
-        <Navigation />
+        <div>
+          <TasteLogo src={Images}></TasteLogo>
+          <Navigation />
+        </div>
         <Route exact path='/' component={Home} />
         <Route path='/lunch-menu' component={LunchMenu} />
         <Route path='/dinner-menu' component={DinnerMenu} />
--- src/Components/Nav/Navigation.js
+++ src/Components/Nav/Navigation.js
@@ -6,20 +6,78 @@ import { Link } from 'react-router-dom';
 
 
 const NavMain = styled.div`
+    
+    .bg-light {
+        background-color: white!important;
+        border-top: 1px solid lightgrey;
+        border-bottom: 1px solid lightgrey;
+        padding: 0;
+    }
+
+    .navbar-light .navbar-toggler {
+        color: rgba(0,0,0,.5);
+        border-color: white;
+        justify-content: center;
+        width: 55px;
+        margin: auto;
+        outline: none;
+        padding: 0;
+    }
+
     p {
         margin-bottom: -1px;
         font-weight: bolder !important;
         text-decoration: none !important;
     }
+
     a {
-        color: #454444;
+        color: #454444 !important;
         text-decoration: none !important;
         font-weight: bolder !important;
         font-size: 16px;
         font-family: 'Open Sans', sans-serif;
+        margin: 0 20px !important;
+        @media only screen 
+and (min-device-width : 320px) 
+and (max-device-width : 568px) { 
+        font-size: 20px;
+}
+        @media only screen 
+    and (device-width : 375px) 
+    and (device-height : 812px) 
+    and (-webkit-device-pixel-ratio : 3) { 
+        font-size: 22px;
+    }
         a:hover {
         color: rgb(111, 179, 77) !important;
         text-decoration: none !important;
+    }
+    }
+
+    @media (min-width: 992px) {
+    .navbar-expand-lg .navbar-collapse {
+        display: flex!important;
+        flex-basis: auto; 
+        justify-content: center !important
+    }
+    }
+
+    @media only screen 
+and (min-device-width : 320px) 
+and (max-device-width : 568px) {
+    .navbar-light .navbar-toggler-icon {
+    background-image: url(data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='30' height='30' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%280, 0, 0, 0.5%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e);
+    font-size: 25px;
+    }
+    }
+
+    @media only screen 
+    and (device-width : 375px) 
+    and (device-height : 812px) 
+    and (-webkit-device-pixel-ratio : 3) {
+    .navbar-light .navbar-toggler-icon {
+    background-image: url(data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='30' height='30' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%280, 0, 0, 0.5%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e);
+    font-size: 30px;
     }
     }
 `;
@@ -27,13 +85,9 @@ const NavMain = styled.div`
 const Navigation = () => {
     return (
         <NavMain>
-            <Navbar bg="light" expand="lg">
-                {/* <Navbar.Brand href="/"><h1>Taste of the Himalayas</h1></Navbar.Brand> */}
+            <Navbar id="links" bg="light" expand="lg">
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        {/* <img src='https://cdn.doordash.com/media/restaurant/cover/TasteoftheHimalayas_2633_Sausalito_CA.png'></img> */}
-                    </Nav>
                     <ReactBooStrap.Nav>
                         <ReactBooStrap.Nav.Link><p className='home-nav'><Link to='/'>Home</Link></p></ReactBooStrap.Nav.Link>
                         <ReactBooStrap.NavDropdown title={<a>Menu</a>} id="basic-nav-dropdown">
