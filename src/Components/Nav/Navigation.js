62361beccd9912a5ddb934c3f5dc8626b9d378d3
Tashi Woeser <tashiwoeser@Tashis-MacBook-Pro.local>
2020-12-02 19:31:36 -0800

    changed navbar styling

--- src/Components/Nav/Navigation.js
+++ src/Components/Nav/Navigation.js
@@ -7,25 +7,25 @@ import PopUp from "../PopUp/PopUp";
 
 const NavMain = styled.div`
   #links {
-    background-image: url('https://images.unsplash.com/photo-1509359149003-657ef23eaf04?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80');
-    border: 1px solid #0c0c0c;
+    background: #a04545 !important;
+    border-top: 1px solid #a04545;
+    border-bottom: 1px solid #a04545;
   }
 
   .modal-footer {
-      padding: 0!important;
-    }
+    padding: 0 !important;
+  }
 
-  .navbar-light .navbar-toggler-icon {
-    font-size: 17px !important;
-    color: red !important;
-}
-
-  .navbar-toggler-icon {
-    color: red !important;
+  .dropdown-item:hover {
+    background: #a04545!important;
   }
 
   .dropdown-menu {
     padding: 0 !important;
+    background: #a04545 !important;
+    border-top: 1px solid #a04545;
+    border-radius: 0;
+    text-align: center !important;
   }
 
   #basic-nav-dropdown:hover {
@@ -33,7 +33,7 @@ const NavMain = styled.div`
   }
 
   .drop-down-links a {
-    color: black !important;
+    color: white !important;
   }
 
   .navbar-light .navbar-nav .nav-link {
@@ -71,7 +71,7 @@ const NavMain = styled.div`
     text-decoration: none !important;
     font-weight: bolder !important;
     font-size: 20px;
-    font-family: 'Barlow Condensed', sans-serif !important;
+    font-family: "Barlow Condensed", sans-serif !important;
 
     @media only screen and (min-device-width: 320px) and (max-device-width: 568px) {
       font-size: 20px;
@@ -99,13 +99,13 @@ const NavMain = styled.div`
         data:image/svg + xml,
         %3csvgxmlns="http://www.w3.org/2000/svg"width="30"height="30"viewBox="0 0 30 30"%3e%3cpathstroke="rgba%280, 0, 0, 0.5%29"stroke-linecap="round"stroke-miterlimit="10"stroke-width="2"d="M4 7h22M4 15h22M4 23h22"/%3e%3c/svg%3e
       );
-      font-size: 25px;
+      font-size: 20px;
     }
     .navbar {
       border: none !important;
     }
     .modal-footer {
-      padding: 0!important;
+      padding: 0 !important;
     }
   }
 
@@ -115,7 +115,7 @@ const NavMain = styled.div`
         data:image/svg + xml,
         %3csvgxmlns="http://www.w3.org/2000/svg"width="30"height="30"viewBox="0 0 30 30"%3e%3cpathstroke="rgba%280, 0, 0, 0.5%29"stroke-linecap="round"stroke-miterlimit="10"stroke-width="2"d="M4 7h22M4 15h22M4 23h22"/%3e%3c/svg%3e
       );
-      font-size: 30px;
+      font-size: 20px;
     }
   }
 `;
@@ -138,17 +138,17 @@ const Navigation = () => {
               id="basic-nav-dropdown"
             >
               <ReactBooStrap.NavDropdown.Item>
-                <p className='drop-down-links'>
+                <p className="drop-down-links">
                   <Link to="/lunch-menu">Lunch Menu</Link>
                 </p>
               </ReactBooStrap.NavDropdown.Item>
               <ReactBooStrap.NavDropdown.Item>
-                <p className='drop-down-links'>
+                <p className="drop-down-links">
                   <Link to="/dinner-menu">Dinner Menu</Link>
                 </p>
               </ReactBooStrap.NavDropdown.Item>
               <ReactBooStrap.NavDropdown.Item>
-                <p className='drop-down-links'>
+                <p className="drop-down-links">
                   <Link to="/wine-list">Wine List</Link>
                 </p>
               </ReactBooStrap.NavDropdown.Item>
