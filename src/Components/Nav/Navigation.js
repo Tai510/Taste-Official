fac8c657e4be22095bd95d0211ad9226e35e7444
Tashi Woeser <tashiwoeser@Tashis-MacBook-Pro.local>
2020-12-02 19:58:30 -0800

    more styling on navbar

--- src/Components/Nav/Navigation.js
+++ src/Components/Nav/Navigation.js
@@ -99,13 +99,16 @@ const NavMain = styled.div`
         data:image/svg + xml,
         %3csvgxmlns="http://www.w3.org/2000/svg"width="30"height="30"viewBox="0 0 30 30"%3e%3cpathstroke="rgba%280, 0, 0, 0.5%29"stroke-linecap="round"stroke-miterlimit="10"stroke-width="2"d="M4 7h22M4 15h22M4 23h22"/%3e%3c/svg%3e
       );
-      font-size: 20px;
+      font-size: 16px;
     }
     .navbar {
       border: none !important;
     }
     .modal-footer {
       padding: 0 !important;
+    }
+    .navbar-light .navbar-toggler {
+      margin-left: 20px;
     }
   }
 
