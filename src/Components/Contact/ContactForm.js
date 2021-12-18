5531e2a865418695e92549fc16e5807ead0a88bf
Tashi Woeser <tashiwoeser@Tashis-MacBook-Pro.local>
2021-12-18 01:11:50 -0800

    testing deploy

--- src/Components/Contact/ContactForm.js
+++ src/Components/Contact/ContactForm.js
@@ -64,7 +64,7 @@ const ContactForm = () => {
         {/* <button className='contact-submit-button' disabled={(!name, !email, !message)} type="submit">
           Submit
         </button> */}
-        <button className='contact-submit-button' type="submit" value="Send Message">Test</button>
+        <button className='contact-submit-button' type="submit" value="Send Message">Worked!!</button>
       </form>
     </div>
   );
