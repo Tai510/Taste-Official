75c5dd64ef53d12c488ef458aebfa9233e16e138
Tashi Woeser <tashiwoeser@Tashis-MacBook-Pro.local>
2021-12-18 00:44:23 -0800

    email working

--- src/Components/Contact/ContactForm.js
+++ src/Components/Contact/ContactForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { db } from "../../firebase";
 import firebase from "firebase/app";
+import emailjs from 'emailjs-com';
 
 const ContactForm = () => {
   const [name, setName] = useState("");
@@ -10,25 +11,33 @@ const ContactForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    
+    // db.collection("messages")
+    //   .add({
+    //     name: name,
+    //     email: email,
+    //     message: message,
+    //     timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    //   })
+    //   .then(() => {
+    //     alert("Thank You! We will get back to you.");
+    //   })
+    //   .catch((error) => {
+    //     alert(error.message);
+    //     console.log("Didnt send message!!!");
+    //   });
 
-    db.collection("messages")
-      .add({
-        name: name,
-        email: email,
-        message: message,
-        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-      })
-      .then(() => {
-        alert("Thank You! We will get back to you.");
-      })
-      .catch((error) => {
-        alert(error.message);
-        console.log("Didnt send message!!!");
+      emailjs.sendForm('service_a00mso8', 'template_inhzoec', e.target, 'user_eTVdymUq7lrCAxWKbDBqg')
+      .then((result) => {
+          console.log(result);
+      }, (error) => {
+          console.log(error.text);
       });
+      e.target.reset()
 
-    setName("");
-    setEmail("");
-    setMessage("");
+    // setName("");
+    // setEmail("");
+    // setMessage("");    
   };
 
   return (
@@ -36,22 +45,26 @@ const ContactForm = () => {
       <form className="form-div" onSubmit={handleSubmit}>
         <input
           placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          // value={name}
+          name='name'
+          // onChange={(e) => setName(e.target.value)}
         ></input>
         <input
           placeholder="Enter Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          // value={email}
+          name='email'
+          // onChange={(e) => setEmail(e.target.value)}
         ></input>
         <textarea
           placeholder="Message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
+          // value={message}
+          name='message'
+          // onChange={(e) => setMessage(e.target.value)}
         ></textarea>
-        <button className='contact-submit-button' disabled={(!name, !email, !message)} type="submit">
+        {/* <button className='contact-submit-button' disabled={(!name, !email, !message)} type="submit">
           Submit
-        </button>
+        </button> */}
+        <input type="submit" value="Send Message"></input>
       </form>
     </div>
   );
